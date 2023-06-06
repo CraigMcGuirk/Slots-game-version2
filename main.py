5ad255a1a51870d532a8ceb3fe49ebf0b256b0c7
Craig McGuirk <craig.m@simplex.vc>
2023-06-06 15:12:09 +0300

    Create main function, ask user to input deposit amount

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_deposit():
+    user_input = input("Please input deposit amount: ")
+
+    return user_input
+
+
+def main(): 
+    # Assigning a return value of function "get_deposit" to variable called "deposit" 
+    deposit = get_deposit()
+
+main()

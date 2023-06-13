86ddbe1c205a75ada205c13da4bc438aa81b22e4
Craig McGuirk <craig.m@simplex.vc>
2023-06-13 15:42:18 +0300

    add max_amount variable and change max deposit amount

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 def get_deposit():
+    max_amount = 900
+    
     while True:
-        user_input = input("Please input deposit amount between €1 and €1000: ")
+        user_input = input(f"Please input deposit amount between €1 and {max_amount}: ")
         # Trys to convert user_input to integer
         try: 
             deposit = int(user_input)
-            # Checks if deposit amount is less than or equal to 0 or greater than 1000
-            # If the amount is less than equal to 0 or greater than 1000 it will loop back to the beginning and ask the user to input a valid amount
-            if deposit <= 0 or deposit > 1000: 
-                print("Amount must be a number between 1 and 1000\n")
+            # Checks if deposit amount is less than or equal to 0 or greater than (max_amount)
+            # If the amount is less than equal to 0 or greater than (max_amount) it will loop back to the beginning and ask the user to input a valid amount
+            if deposit <= 0 or deposit > max_amount: 
+                print(f"Amount must be a number between 1 and {max_amount}\n")
                 continue
             # if inputted amount is valid it will return this amount. 
             return deposit
